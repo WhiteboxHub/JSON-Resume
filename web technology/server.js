4358f7d651bc9ce55c4a67f80f25679109b59760
Innovapath Inc <training@whitebox-learning.com>
2024-06-19 21:35:12 +0530

    changed macchiato path to sampleForms

--- web technology/server.js
+++ web technology/server.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/forms", (req, res) => {
-  res.sendFile(__dirname + "/public/sampleForms/form.html");
+  res.sendFile(__dirname + "/public/sampleForms/macchiato.html");
 });
 
 // Define a route to handle form submissions
