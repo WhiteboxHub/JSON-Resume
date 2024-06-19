8e47fd096253f6d811c506941b9cd5af98083844
Innovapath Inc <training@whitebox-learning.com>
2024-06-19 21:48:47 +0530

    deleted json-resume folder

--- web technology/tailwind.config.js
+++ web technology/tailwind.config.js
@@ -1,13 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
-// tailwind.config.js
 module.exports = {
   content: [
     './*.html',
